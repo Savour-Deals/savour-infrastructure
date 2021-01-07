f857ef21ae8e4e2f2080c3adc9124919c9ae87d0
Brandon Ly <wowbob396@gmail.com>
2021-01-06 18:09:56 -0600

    added database stuff

--- lib/DynamoDBStack.ts
+++ lib/DynamoDBStack.ts
@@ -3,15 +3,15 @@ import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as sst from "@serverless-stack/resources";
 
 export default class DynamoDBStack extends sst.Stack {
-  constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+  constructor(scope: sst.App, id: string, tableName: string, partitionKey: string, props?: sst.StackProps) {
     super(scope, id, props);
     
     const app: any = this.node.root;
 
     const table = new dynamodb.Table(this, "Table", {
+      tableName: tableName,
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // Use on-demand billing mode
-      sortKey: { name: "noteId", type: dynamodb.AttributeType.STRING },
-      partitionKey: { name: "userId", type: dynamodb.AttributeType.STRING },
+      partitionKey: { name: partitionKey, type: dynamodb.AttributeType.STRING },
     });
     
     // Output values
--- lib/SavourDashboardStack.ts
+++ lib/SavourDashboardStack.ts
@@ -1,11 +1,33 @@
 import * as sst from "@serverless-stack/resources";
+import DynamoDBStack from './DynamoDBStack';
+
+interface DynamoDbTableDefintion {
+  tableName: string;
+  partitionKey: string;
+}
+
+const DYNAMO_TABLES: Array<DynamoDbTableDefintion> = [
+  { tableName: 'business_users', partitionKey: 'uid' },
+  { tableName: 'businesses', partitionKey: 'place_id' },
+  { tableName: 'push_table', partitionKey: 'unique_id' },
+  { tableName: 'redirect_urls', partitionKey: 'unique_id' },
+  { tableName: 'subscriber_users', partitionKey: 'mobile_number' },
+  { tableName: 'unclaimed_buttons', partitionKey: 'button_id' },
+
+]
 
 export default class SavourDashboardStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
-    super(scope,id,props)
+    super(scope,id,props);
+
+    this.createDynamoTables(DYNAMO_TABLES);
+
+  }
+
+  createDynamoTables(tables: Array<object>) {
+    for (const table of  tables) {
+      
+    }
   }
 }
 
-function createStack(app: sst.App, environment: string) : sst.Stack {
-  return new SavourDashboardStack(app, environment);
-}
